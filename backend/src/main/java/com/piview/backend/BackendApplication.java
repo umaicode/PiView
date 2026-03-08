958412dd3b288767c75b9bd950bb7973070599db
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2026-03-09 01:22:28 +0900

    BE-feat : JWT 토큰 생성/검증 로직 및 UserPrincipal 구현

--- backend/src/main/java/com/piview/backend/BackendApplication.java
+++ backend/src/main/java/com/piview/backend/BackendApplication.java
@@ -1,8 +1,11 @@
 package com.piview.backend;
 
+import com.piview.backend.global.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
 public class BackendApplication {
     private int stttt_Sttt;
