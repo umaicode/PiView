0cb4491a1b2dc4c28158d70b07543698be86152f
김상지 <139340193+theundergroundt@users.noreply.github.com>
2026-03-24 09:53:36 +0900

    BE-fix : service명 오류 수정

--- backend/src/main/java/com/piview/backend/domain/product/aisummary/service/AiRecommendationService.java
+++ backend/src/main/java/com/piview/backend/domain/product/aisummary/service/AiRecommendationService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class RecommendationService {
+public class AiRecommendationService {
 
   private final MySkinRepository mySkinRepository;
 
